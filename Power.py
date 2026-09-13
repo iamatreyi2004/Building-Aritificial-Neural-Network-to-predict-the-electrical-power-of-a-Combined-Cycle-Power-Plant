ef5f26d29b155dfcf975f49497b11e2fc08bddf3
Atreyi Das <iamatreyi2004@gmail.com>
2026-09-13 11:28:49 +0530

    Update Power.py

--- Power.py
+++ Power.py
@@ -3,24 +3,30 @@ import numpy as np
 import tensorflow as tf
 
 tf.__version__
-
+#Loading dataset
 dataset=pd.read_excel('Folds5x2_pp.xlsx')
+#Seperating predictor and response
 X=dataset.iloc[:,:-1].values
 y=dataset.iloc[:,-1].values
 print(X)
 print(y)
-
+#Dividing data intro train and test set
 from sklearn.model_selection import train_test_split
 X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=0)
 
+#Standardizing predictors
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test=sc.fit_transform(X_test)
 
+#Fitting multiple linear regression
 from sklearn.linear_model import LinearRegression
 lin_reg = LinearRegression()
-lin_reg.fit(X, y)
+lin_reg.fit(X_train, y_train)
+y_pred=lin_reg.predict(X_test)
+r2_score(y_pred,y_test)
+#Fitting polynomial regression
 from sklearn.preprocessing import PolynomialFeatures
 poly_reg = PolynomialFeatures(degree = 4)
 X_poly = poly_reg.fit_transform(X)
@@ -35,6 +41,7 @@ y_pred = lin_reg_2.predict(X_test_poly)
 r2 = r2_score(y_test, y_pred)
 print(r2)
 
+#Fitting regression tree
 from sklearn.tree import DecisionTreeRegressor
 
 tree = DecisionTreeRegressor(
@@ -51,6 +58,7 @@ y_pred = tree.predict(X_test)
 r2 = r2_score(y_test, y_pred)
 print(r2 )
 
+#Fitting XG-Boost
 from xgboost import XGBRegressor
 xgb_model = XGBRegressor(
     n_estimators=100,
@@ -76,7 +84,6 @@ ann.add(tf.keras.layers.Dense(units=10,activation='relu'))#10 neurons in second 
 ann.add(tf.keras.layers.Dense(units=1))
 ann.compile(optimizer='adam',loss='mean_squared_error')
 ann.fit(X_train,y_train,batch_size=32,epochs=100)
-
 y_pred=ann.predict(X_test)
 np.concatenate((y_pred.reshape(len(y_pred),1),y_test.reshape(len(y_test),1)),1)
 print(r2_score(y_test,y_pred))
